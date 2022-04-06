454dcdd452eabaa6a99128f5dbd1a40f6b3d6dce
makarovas <makarofalex@gmail.com>
2022-04-06 15:27:26 +0300

    refactoring

--- src/containers/Main.tsx
+++ src/containers/Main.tsx
@@ -2,19 +2,7 @@ import React, { FC } from "react";
 import { useMain } from "hooks/useMain";
 
 import { MainView } from "view/MainView";
-import { BallotsItemType, NomineeType } from "hooks/useFetchBallots/types";
 
-type MainProps = {
-  ballots: BallotsItemType[];
-  isLoadingBallot: boolean;
-  ballotsError: null;
-  hasBallots: boolean;
-  onSelectNomineeCategory: (
-    categoryId: string,
-    categoryTitle: string
-  ) => (nominee: NomineeType) => void;
-  onSubmitVote: () => void;
-};
 const Main: FC = () => {
   const controller = useMain();
   return <MainView {...controller} />;
